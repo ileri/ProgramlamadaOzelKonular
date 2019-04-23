ea0422157c0d6fc266a5d4b8fc8d52e9b6979da1
ileri <celil.ileri@bil.omu.edu.tr>
2019-04-23 22:43:04 +0300

    add pragma once

--- src/matarith.h
+++ src/matarith.h
@@ -1,3 +1,5 @@
+#pragma once // Kütüphanenin bir defalığına include edilmesini sağlar.
+
 int matrix_add(int* out_matrix, const int *m1, const int *m2, int row_count, int col_count);
 int matrix_sub(int* out_matrix, const int *m1, const int *m2, int row_count, int col_count);
 int matrix_mul(int* out_matrix, int* m1, int* m2, int m1_col_count, int row_count, int m2_col_count);
